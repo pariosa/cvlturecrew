3857d94eb5bc3cfa37fc69721d14e94adc37a812
o k <w0xy@Macbook.local>
2019-09-07 21:38:02 -0400

    fix bundler

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-//import './index.css';
 import App from './js/App';
 import React from 'react';
 import ReactDOM from "react-dom";
-import "semantic-ui-css/semantic.min.css";
-//import index from "./js/index";
+import "semantic-ui-css/semantic.min.css"; 
 import * as serviceWorker from './serviceWorker'; 
-
-//store.firebaseAuthIsReady.then(()=>{
-	ReactDOM.render(
-	     <App />
-	    , document.getElementById('root')
-	);
-//})
+ 
+ReactDOM.render(
+        <App />
+    , document.getElementById('app')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
