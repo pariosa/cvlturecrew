435f1ad6a03a8906d1032f54baeb25f168cd6ffe
o k <w0xy@Macbook.local>
2019-09-07 21:10:18 -0400

    set up node and get the basic configs all ready to go! babelrc, eslintrc, and webpack are configured

---
+++ src/js/App.js
@@ -0,0 +1,17 @@
+import './App.css';   
+import React, { Component } from 'react';
+
+
+class App extends Component {
+ 
+  render() {     
+      return(
+        <div className="Layout">   
+            Here is the future home of Cvlture Crew!
+        </div>
+      )
+    } 
+}
+//<Header />
+//<MainContent /> 
+export default App;
