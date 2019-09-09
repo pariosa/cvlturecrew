3bd2ae2add628e680afcc2544b0b5b4a7d2083a5
pariosa <ariosa@gmail.com>
2019-09-09 16:24:02 -0400

    add particle background on tabs

--- src/js/App.js
+++ src/js/App.js
@@ -6,7 +6,7 @@ import About from './About';
 import Home from './Home';
 import Contact from './Contact';
 import Services from './Services';
-
+import Particles from 'react-particles-js'
 class App extends Component {
  
   render() {     
@@ -16,26 +16,233 @@ class App extends Component {
             <Tabs>
             <div label="Home">
               <div className="content-bg">
+              <Particles
+                params={{
+                  "particles": {
+                      "number": {
+                          "value": 160,
+                          "density": {
+                              "enable": false
+                          }
+                      },
+                      "size": {
+                          "value": 3,
+                          "random": true,
+                          "anim": {
+                              "speed": 4,
+                              "size_min": 0.3
+                          }
+                      },
+                      "line_linked": {
+                          "enable": false
+                      },
+                      "move": {
+                          "random": true,
+                          "speed": 1,
+                          "direction": "top",
+                          "out_mode": "out"
+                      }
+                  },
+                  "interactivity": {
+                      "events": {
+                          "onhover": {
+                              "enable": true,
+                              "mode": "bubble"
+                          },
+                          "onclick": {
+                              "enable": true,
+                              "mode": "repulse"
+                          }
+                      },
+                      "modes": {
+                          "bubble": {
+                              "distance": 250,
+                              "duration": 2,
+                              "size": 0,
+                              "opacity": 0
+                          },
+                          "repulse": {
+                              "distance": 400,
+                              "duration": 4
+                          }
+                      }
+                  }
+              }} />
                 <Home/>
               </div>
             </div>
             <div label="About">
               <div className="content-bg">
+              <Particles
+                params={{
+                  "particles": {
+                      "number": {
+                          "value": 160,
+                          "density": {
+                              "enable": false
+                          }
+                      },
+                      "size": {
+                          "value": 3,
+                          "random": true,
+                          "anim": {
+                              "speed": 4,
+                              "size_min": 0.3
+                          }
+                      },
+                      "line_linked": {
+                          "enable": false
+                      },
+                      "move": {
+                          "random": true,
+                          "speed": 1,
+                          "direction": "top",
+                          "out_mode": "out"
+                      }
+                  },
+                  "interactivity": {
+                      "events": {
+                          "onhover": {
+                              "enable": true,
+                              "mode": "bubble"
+                          },
+                          "onclick": {
+                              "enable": true,
+                              "mode": "repulse"
+                          }
+                      },
+                      "modes": {
+                          "bubble": {
+                              "distance": 250,
+                              "duration": 2,
+                              "size": 0,
+                              "opacity": 0
+                          },
+                          "repulse": {
+                              "distance": 400,
+                              "duration": 4
+                          }
+                      }
+                  }
+              }} />
                 <About/>
               </div>
             </div>
             <div label="Services">
               <div className="content-bg">
+                <Particles
+                  params={{
+                    "particles": {
+                        "number": {
+                            "value": 160,
+                            "density": {
+                                "enable": false
+                            }
+                        },
+                        "size": {
+                            "value": 3,
+                            "random": true,
+                            "anim": {
+                                "speed": 4,
+                                "size_min": 0.3
+                            }
+                        },
+                        "line_linked": {
+                            "enable": false
+                        },
+                        "move": {
+                            "random": true,
+                            "speed": 1,
+                            "direction": "top",
+                            "out_mode": "out"
+                        }
+                    },
+                    "interactivity": {
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": "bubble"
+                            },
+                            "onclick": {
+                                "enable": true,
+                                "mode": "repulse"
+                            }
+                        },
+                        "modes": {
+                            "bubble": {
+                                "distance": 250,
+                                "duration": 2,
+                                "size": 0,
+                                "opacity": 0
+                            },
+                            "repulse": {
+                                "distance": 400,
+                                "duration": 4
+                            }
+                        }
+                    }
+                }} />
                 <Services/>
               </div>
             </div>
             <div label="Contact">
               <div className="content-bg">
+              <Particles
+                  params={{
+                    "particles": {
+                        "number": {
+                            "value": 160,
+                            "density": {
+                                "enable": false
+                            }
+                        },
+                        "size": {
+                            "value": 3,
+                            "random": true,
+                            "anim": {
+                                "speed": 4,
+                                "size_min": 0.3
+                            }
+                        },
+                        "line_linked": {
+                            "enable": false
+                        },
+                        "move": {
+                            "random": true,
+                            "speed": 1,
+                            "direction": "top",
+                            "out_mode": "out"
+                        }
+                    },
+                    "interactivity": {
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": "bubble"
+                            },
+                            "onclick": {
+                                "enable": true,
+                                "mode": "repulse"
+                            }
+                        },
+                        "modes": {
+                            "bubble": {
+                                "distance": 250,
+                                "duration": 2,
+                                "size": 0,
+                                "opacity": 0
+                            },
+                            "repulse": {
+                                "distance": 400,
+                                "duration": 4
+                            }
+                        }
+                    }
+                }} />
                <Contact/>
               </div>
             </div>
             </Tabs>
-
          </div>
       )
     } 
--- src/js/Header.js
+++ src/js/Header.js
@@ -8,7 +8,34 @@ class Header extends Component {
   render() {     
       return(
         <div className="Header">   
-          <Particles height="200px"/>
+          <Particles height="200px"
+      
+            params={{ 
+              particles: {
+                "number": {
+                    "value": 60
+                },
+                "size": {
+                    "value": 1
+                },
+              },
+              "interactivity": {
+                "events": {
+                    "onhover": {
+                        "enable": true,
+                        "mode": "repulse"
+                    }
+                }
+              },
+              line_linked: {
+                shadow: {
+                  enable: true,
+                  color: "#ccccff",
+                  blur: 10
+                }
+              }
+            }}
+          />
           <img src={logo} style={{maxWidth:'600px', marginTop:'-250px'}}></img>
         </div>
       )
