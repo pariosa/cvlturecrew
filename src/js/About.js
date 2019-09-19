b762d0ed4f6cac766e21dfd8075cbb180608b4aa
pariosa <ariosa@gmail.com>
2019-09-18 22:53:46 -0400

    text update ona bt

--- src/js/About.js
+++ src/js/About.js
@@ -21,8 +21,7 @@ class About extends Component {
     const { photoIndex, isOpen } = this.state;
         return(
             <div className="About" style={{fontSize:'22px',lineHeight:'1em'}}>
-           Every company and collective has a story. Our narrative begins in the underground music & arts communities of Cincinnati, OH. Owner/Operator, Jon G Ferreri Jr, launched our collective during his own creative journey as a Conscious Hip Hop artist; infatuated with Jazz, EDM, Funk, and visual arts. Knowing it couldn't be managed alone, he was later joined by CJ Schiff, a fellow Cincinnati native who was equally in-love with the Night Life and the creative wonders that fuel it's fire. Our family is ever-growing and determined to bring you boundless insight into the creative world, as well as offer platforms and services to take creators to their next level in their careers/passions. 
-           	<br/><br/>
+Every company and collective has a story. Our narrative begins in the underground music & arts communities of Cincinnati, OH. Owner/Operator, Jon G Ferreri Jr, launched our collective during his own creative journey as a Conscious Hip Hop artist; infatuated with Jazz, EDM, Funk, and visual arts. Knowing it couldn't be managed alone, he was later joined by CJ Schiff, Evan Shelton, J.T., That Guy Lighting, and several others; fellow Cincinnati natives who are equally in-love with the creative wonders that fuel our Art & Music scenes. Our family is ever-growing and determined to bring you boundless insight into the creative world, as well as offer platforms and services to take creators to their next level in their careers/passions.            	<br/><br/>
 <div style={{textAlign:'center'}}>
 The real magic is our incredible crew of creators, and you, our family who joins the experience.
 <br/><br/>
